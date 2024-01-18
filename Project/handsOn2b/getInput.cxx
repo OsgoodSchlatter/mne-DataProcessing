30cfc223a18cbf8e1cd8efe1126d23ccc2a2b001
Osgood <eloi.besnard@telecom-sudparis.eu>
2024-01-18 13:42:05 +0100

    getInput

---
+++ Project/handsOn2b/getInput.cxx
@@ -0,0 +1,17 @@
+#include <iostream>
+
+#include <stdio.h>
+
+using namespace std;
+
+int main()
+{
+    int x;
+    cout << "Type a number: "; // Type a number and press enter
+    cin >> x;
+    while (x != 0)
+    {
+        cout << " Continue \n";
+        cin >> x;
+    }
+}
