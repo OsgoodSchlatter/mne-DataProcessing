88300613092ade9d8e499942485cf19468c3d4ad
Osgood <eloi.besnard@telecom-sudparis.eu>
2024-02-08 14:52:34 +0100

    1st commit test on git practice (handson4c)

---
+++ Project/handsOn4b/git_practice.cxx
@@ -0,0 +1,43 @@
+// Compile
+// g++ -Wall -std=c++11 comptonTest.cxx -o comptonTest && ./comptonTest
+
+#include <cmath>
+#include <vector>
+#include <iostream>
+
+using namespace std;
+
+float computeDefaultAngle(float ee, float eg)
+{
+    const float electronMassKeV = 511;
+    float angle1 = 1 - electronMassKeV / eg + electronMassKeV / (ee + eg);
+    if (angle1 < 1 && angle1 > -1)
+    {
+        return std::acos(angle1);
+    }
+    return -100.0;
+}
+
+int main()
+{
+    const vector<float> eeVector = {
+        67941.8672, 148265.750, 221265.891, 211372.953, 144909.406, 208374.766, 180113.234, 113415.320,
+        244529.219, 154693.859, 57815.7266, 48517.4297, 111427.633, 44456.5195, 122388.523, 50021.0742,
+        211402.500, 237187.156, 26450.9902, 183725.344};
+    const vector<float> egVector = {
+        156211.781, 165564.312, 44844.2344, 30251.5898, 91047.4219, 184591.281, 243864.500, 239624.828,
+        47976.1289, 162145.359, 7380.48486, 71683.7891, 241140.562, 95328.1484, 132011.188, 78232.0000,
+        18459.6875, 39155.9609, 197348.500, 182669.297};
+
+    cout << "Test evaluation of Compton scattering angle\n";
+
+    for (size_t idx = 0; idx < eeVector.size(); ++idx)
+    {
+        const float ee = eeVector[idx]; // Energy of recoil electron
+        const float eg = egVector[idx]; // Energy of scattered gamma ray
+        cout
+            << ee << ' ' << eg << ':'
+            << ' ' << computeDefaultAngle(ee, eg)
+            << '\n';
+    }
+}
